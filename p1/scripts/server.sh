f657c157a330ecaaedb5b5f3b6c7f03c39a4be7f
Boumlik Brahim <boumlik01brahim@gmail.com>
2024-09-20 16:43:05 +0100

    part one

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,8 +1,9 @@
-sudo -i
+#!/bin/bash
+
 apt-get update
 apt-get install -y curl
 
 curl -sfL https://get.k3s.io | sh -
 echo "Sleeping for 5 seconds to wait for k3s to start"
 sleep 5
-cp /var/lib/rancher/k3s/server/token /vagrant_shared
+cp /var/lib/rancher/k3s/server/node-token /vagrant_shared
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,9 +1,9 @@
-sudo -i
+#!/bin/bash
+
 apt-get update
 apt-get install -y curl
 
 SERVER_IP=192.168.56.110
-K3S_TOKEN=$(cat /vagrant_shared/token)
-
+K3S_TOKEN=$(cat /vagrant_shared/node-token)
 
 curl -sfL https://get.k3s.io | K3S_URL=https://$SERVER_IP:6443 K3S_TOKEN=$K3S_TOKEN  sh -
